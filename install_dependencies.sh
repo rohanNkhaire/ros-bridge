d34aa29cd7b6d113db5ebe02e5f3703aa032bb93
rohanNkhaire <rohanNkhaire@gmail.com>
2023-12-28 17:14:54 -0700

    Fixed dependencies for galactic

--- install_dependencies.sh
+++ install_dependencies.sh
@@ -34,7 +34,6 @@ sudo apt-get install --no-install-recommends -y \
     python$PYTHON_SUFFIX-wstool \
     python$PYTHON_SUFFIX-opencv \
     ros-$ROS_DISTRO-ackermann-msgs \
-    ros-$ROS_DISTRO-derived-object-msgs \
     ros-$ROS_DISTRO-cv-bridge \
     ros-$ROS_DISTRO-vision-opencv \
     ros-$ROS_DISTRO-rqt-image-view \
